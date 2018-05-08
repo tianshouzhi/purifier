2940e6b9eb99346118c35cf17de168bd5bd102c1
“tianshouzhi” <tianshouzhi@126.com>
2018-05-08 19:14:29 +0800

    create purifier-web module

---
+++ purifier-web/src/main/java/com/tianshouzhi/purifier/PurifierWebApplication.java
@@ -0,0 +1,12 @@
+package com.tianshouzhi.purifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurifierWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurifierWebApplication.class, args);
+	}
+}
---
+++ purifier-web/src/test/java/com/tianshouzhi/purifier/PurifierWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tianshouzhi.purifier;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PurifierWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
